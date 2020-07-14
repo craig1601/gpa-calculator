d3f68a9173b77310511fb1ecbec4e1dc0c063681
craig1601 <craig1601@gmail.com>
2020-07-14 22:25:34 +0530

    Updates

---
+++ precache-manifest.f330b2337cb06566d8829d9a8f703600.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f985f701a0393f50a760ca730c0af2d",
+    "url": "/gpa-calculator/index.html"
+  },
+  {
+    "revision": "3b23fe1cac423f497df4",
+    "url": "/gpa-calculator/static/css/main.e87f9b6d.chunk.css"
+  },
+  {
+    "revision": "b490cba6aa469782d451",
+    "url": "/gpa-calculator/static/js/2.272a2c7c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gpa-calculator/static/js/2.272a2c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b23fe1cac423f497df4",
+    "url": "/gpa-calculator/static/js/main.ba77669e.chunk.js"
+  },
+  {
+    "revision": "b8d9aa72d0b9eac85e5f",
+    "url": "/gpa-calculator/static/js/runtime-main.4cfde7f1.js"
+  }
+]);
